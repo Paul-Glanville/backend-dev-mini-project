81aa6494c2c1628fae2f5210bf01ec8aae5677c9
Paul-Glanville <glanville49@googlemail.com>
2021-04-24 16:05:31 +0000

    updated mysql-from-python.py file

--- mysql-from-python.py
+++ mysql-from-python.py
@@ -1,4 +1,5 @@
 import os
+import datetime
 import pymysql
 
 # Get username from workspace
@@ -11,11 +12,10 @@ connection = pymysql.connect(host='localhost',
 
 try:
     # Run a query
-    with connection.cursor(pymysql.cursors.DictCursor) as cursor:
-        sql = "select * from Genre;"
-        cursor.execute(sql)
-        for row in cursor:
-            print(row)
+    with connection.cursor() as cursor:
+        row = ("Bob", 21, "1990-02-06 23:04:56")
+        cursor.execute("INSERT INTO Friends values (%s, %s, %s);", row)
+        connection.commit()
 finally:
     # Close the connection, regardless of whether the above was successful
     connection.close()
